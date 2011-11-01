b9aea28477beda62285cb7183345a07e1a152938
Chris Thunes <cmthunes@ncsu.edu>
2011-11-01 10:53:18 -0400

    Wrap cv.h to avoid warnings/errors about unused functions that OpenCV defines directly in their headers files

---
+++ include/svr/cv.h
@@ -0,0 +1,10 @@
+
+#ifndef __SVR_CV_H
+#define __SVR_CV_H
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+#include <cv.h>
+#pragma GCC diagnostic pop
+
+#endif // #ifndef __SVR_CV_H
