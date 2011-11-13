2dab5448b32039eab50c684de3db1dfeee80f0a5
Mr. Seawolf <seawolf@seawolf.ncsurobotics.com>
2011-11-13 12:59:39 -0500

    Provide fallback for libjpeg62

--- lib/encodings/jpeg.c
+++ lib/encodings/jpeg.c
@@ -47,6 +47,87 @@ typedef struct {
     unsigned long bytes_received;
 } SVR_JpegDecoder;
 
+#if JPEG_LIB_VERSION < 80
+
+/* The following code is borrowed from libjpeg8 */
+
+METHODDEF(void)
+init_mem_source (j_decompress_ptr cinfo)
+{
+    /* no work necessary here */
+}
+
+METHODDEF(boolean)
+fill_mem_input_buffer (j_decompress_ptr cinfo)
+{
+    static JOCTET mybuffer[4];
+
+    /* Insert a fake EOI marker */
+    mybuffer[0] = (JOCTET) 0xFF;
+    mybuffer[1] = (JOCTET) JPEG_EOI;
+
+    cinfo->src->next_input_byte = mybuffer;
+    cinfo->src->bytes_in_buffer = 2;
+
+    return TRUE;
+}
+
+METHODDEF(void)
+skip_input_data (j_decompress_ptr cinfo, long num_bytes)
+{
+    struct jpeg_source_mgr * src = cinfo->src;
+
+    /* Just a dumb implementation for now.  Could use fseek() except
+     * it doesn't work on pipes.  Not clear that being smart is worth
+     * any trouble anyway --- large skips are infrequent.
+     */
+    if (num_bytes > 0) {
+        while (num_bytes > (long) src->bytes_in_buffer) {
+            num_bytes -= (long) src->bytes_in_buffer;
+            (void) (*src->fill_input_buffer) (cinfo);
+            /* note we assume that fill_input_buffer will never return FALSE,
+             * so suspension need not be handled.
+             */
+        }
+        src->next_input_byte += (size_t) num_bytes;
+        src->bytes_in_buffer -= (size_t) num_bytes;
+    }
+}
+
+METHODDEF(void)
+term_source (j_decompress_ptr cinfo)
+{
+    /* no work necessary here */
+}
+
+METHODDEF(void)
+jpeg_mem_src (j_decompress_ptr cinfo,
+              unsigned char * inbuffer, unsigned long insize)
+{
+    struct jpeg_source_mgr * src;
+
+    /* The source object is made permanent so that a series of JPEG images
+     * can be read from the same buffer by calling jpeg_mem_src only before
+     * the first one.
+     */
+    if (cinfo->src == NULL) {/* first time for this JPEG object? */
+        cinfo->src = (struct jpeg_source_mgr *)
+            (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo, JPOOL_PERMANENT,
+                                        sizeof(struct jpeg_source_mgr));
+    }
+
+    src = cinfo->src;
+    src->init_source = init_mem_source;
+    src->fill_input_buffer = fill_mem_input_buffer;
+    src->skip_input_data = skip_input_data;
+    src->resync_to_restart = jpeg_resync_to_restart; /* use default method */
+    src->term_source = term_source;
+    src->bytes_in_buffer = (size_t) insize;
+    src->next_input_byte = (JOCTET *) inbuffer;
+}
+
+#endif
+
 METHODDEF(void) init_svr_destination(j_compress_ptr cinfo) {
     SVR_JpegEncoder* private_data = (SVR_JpegEncoder*) cinfo->dest;
 
