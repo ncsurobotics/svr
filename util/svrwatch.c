8447b02ac0567d2cbe87134059dfa97839179cec
Chris Thunes <cmthunes@ncsu.edu>
2012-07-14 18:29:22 -0400

    Make svrwatch --all poll for new sources

--- util/svrwatch.c
+++ util/svrwatch.c
@@ -4,20 +4,25 @@
 
 #include <highgui.h>
 
-static void svrwatch_usage(const char* argv0);
+#define POLLING_FREQ 5
+
+static int quality = 70;
+static bool list_stale = false;
+static pthread_mutex_t list_stale_lock = PTHREAD_MUTEX_INITIALIZER;
 
 static void svrwatch_usage(const char* argv0) {
-    printf("Usage: %s [-hd] [-s ADDRESS] SOURCE_NAME...\n"
+    printf("Usage: %s [-hdra] [-q QUALITY] [-s ADDRESS] SOURCE_NAME...\n"
            "Seawolf Video Router Stream Watcher\n"
            "\n"
            "  -h, --help                            Show this help message\n"
            "  -d, --debug                           Enable debugging\n"
            "  -s, --server=ADDRESS                  Address of SVR server\n"
            "  -r, --raw                             Use raw encoding (default is JPEG)\n"
-           "  -q, --quality=VALUE                   JPEG stream quality\n", argv0);
+           "  -q, --quality=VALUE                   JPEG stream quality\n"
+           "  -a, --all                             Watch all streams\n", argv0);
 }
 
-static SVR_Stream* svrwatch_open_stream(const char* source_name, int quality) {
+static SVR_Stream* svrwatch_open_stream(const char* source_name) {
     SVR_Stream* stream;
     char* encoding;
 
@@ -46,16 +51,71 @@ static SVR_Stream* svrwatch_open_stream(const char* source_name, int quality) {
     return stream;
 }
 
-int main(int argc, char** argv) {
+static List* svrwatch_streams_list(Dictionary* streams) {
+    List* source_names = Dictionary_getKeys(streams);
+    char* source_name;
+    List* stream_list = List_new();
+
+    for(int i = 0; (source_name = List_get(source_names, i)) != NULL; i++) {
+        List_append(stream_list, Dictionary_get(streams, source_name));
+    }
+
+    List_destroy(source_names);
+
+    return stream_list;
+}
+
+static bool svrwatch_open_new(Dictionary* streams) {
+    SVR_Stream* stream;
     List* sources;
+    int stream_count;
+    char* source_name;
+    bool new_stream = false;
+
+    sources = SVR_getSourcesList();
+    stream_count = List_getSize(sources);
+
+    for(int i = 0; i < stream_count; i++) {
+        source_name = List_get(sources, i);
+        source_name = source_name + 2;
+
+        if(Dictionary_exists(streams, source_name) == false) {
+            stream = svrwatch_open_stream(source_name);
+            Dictionary_set(streams, source_name, stream);
+            new_stream = true;
+        }
+    }
+
+    SVR_freeSourcesList(sources);
+    return new_stream;
+}
+
+static void* svrwatch_polling_thread(void* _streams) {
+    Dictionary* streams = (Dictionary*) _streams;
+
+    while(true) {
+        if(svrwatch_open_new(streams)) {
+            pthread_mutex_lock(&list_stale_lock);
+            list_stale = true;
+            pthread_mutex_unlock(&list_stale_lock);
+        }
+        Util_usleep(1.0/POLLING_FREQ);
+    }
+
+    return NULL;
+}
+
+int main(int argc, char** argv) {
     IplImage* frame;
-    SVR_Stream** streams;
+    Dictionary* streams;
+    SVR_Stream* stream;
+    List* streams_list;
     int stream_count;
     int opt, indexptr;
     char* source_name;
     bool watch_all = false;
     bool raw = false;
-    int quality = 70;
+    pthread_t polling_thread;
 
     struct option long_options[] = {
         {"help", 0, NULL, 'h'},
@@ -125,44 +185,49 @@ int main(int argc, char** argv) {
         quality = -1;
     }
 
+    streams = Dictionary_new();
+
     if(watch_all) {
-        sources = SVR_getSourcesList();
-        stream_count = List_getSize(sources);
-        streams = malloc(sizeof(SVR_Stream*) * stream_count);
-
-        for(int i = 0; i < stream_count; i++) {
-            source_name = List_get(sources, i);
-            source_name = source_name + 2;
-            streams[i] = svrwatch_open_stream(source_name, quality);
-        }
-
-        SVR_freeSourcesList(sources);
+        svrwatch_open_new(streams);
+        pthread_create(&polling_thread, NULL, svrwatch_polling_thread, streams);
     } else {
         stream_count = argc - optind;
-        streams = malloc(sizeof(SVR_Stream*) * stream_count);
         for(int i = 0; i < stream_count; i++) {
             source_name = argv[optind + i];
-            streams[i] = svrwatch_open_stream(source_name, quality);
+            stream = svrwatch_open_stream(source_name);
+
+            Dictionary_set(streams, source_name, stream);
         }
     }
 
+    streams_list = svrwatch_streams_list(streams);
+    stream_count = List_getSize(streams_list);
+
     printf("Press 'q' to quit.\n");
 
-    while(cvWaitKey(10) != 'q' && stream_count > 0) {
-        SVR_Stream_sync();
+    while(cvWaitKey(10) != 'q' && (stream_count > 0 || watch_all)) {
+        pthread_mutex_lock(&list_stale_lock);
+        if(list_stale) {
+            list_stale = false;
+            List_destroy(streams_list);
+            streams_list = svrwatch_streams_list(streams);
+            stream_count = List_getSize(streams_list);
+        }
+        pthread_mutex_unlock(&list_stale_lock);
 
         for(int i = 0; i < stream_count; i++) {
-            frame = SVR_Stream_getFrame(streams[i], false);
+            stream = List_get(streams_list, i);
+            frame = SVR_Stream_getFrame(stream, false);
 
             if(frame) {
-                cvShowImage(Util_format("%s:%s", streams[i]->source_name, streams[i]->stream_name), frame);
-                SVR_Stream_returnFrame(streams[i], frame);
-            } else if(SVR_Stream_isOrphaned(streams[i])) {
-                fprintf(stderr, "Stream \"%s\" orphaned\n", streams[i]->source_name);
-                SVR_Stream_destroy(streams[i]);
-                streams[i] = streams[stream_count - 1];
-                stream_count--;
-                i--;
+                cvShowImage(Util_format("%s:%s", stream->source_name, stream->stream_name), frame);
+                SVR_Stream_returnFrame(stream, frame);
+            } else if(SVR_Stream_isOrphaned(stream)) {
+                fprintf(stderr, "Stream \"%s\" orphaned\n", stream->source_name);
+                cvDestroyWindow(Util_format("%s:%s", stream->source_name, stream->stream_name));
+                Dictionary_remove(streams, stream->source_name);
+                SVR_Stream_destroy(stream);
+                list_stale = true;
             }
         }
     }
